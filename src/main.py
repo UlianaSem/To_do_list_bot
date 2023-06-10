caf6456039ac1ba853863e2dc012c8e513f2a1e3
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-10 16:39:59 +0500

    Update main.py - add docstrings to functions - change functions names

--- src/main.py
+++ src/main.py
@@ -1,93 +1,105 @@
-import telebot
-from random import choice
+import os
 from datetime import datetime, timedelta
-import re
+from random import choice
 
-token = ''
-bot = telebot.TeleBot(token)
+import telebot
+
+import utils
+
+
+TOKEN = os.getenv('TO_DO_BOT_API')
 HELP = '''/help - вывести список доступных команд
 /add - добавить новую задачу в список в формате "/add - дата - задача"
 /show - напечатать все добавленные задачи на заданную дату в формате "/show - дата"
 /random - добавить случайную задачу на сегодняшнюю дату'''
-tasks = {}
 RANDOM_TASKS = ["заказать еду", "посмотреть фильм", "почитать книгу", "сходить в ресторан", 'встретиться с друзьями',
                 'завести кошку', 'завести собаку', 'выпить шампанского', 'разобрать шкаф', 'сходить на шоппинг',
                 'устроить внеплановую тренировку']
-months = {'января': 1, 'февраля': 2, 'марта': 3, 'апреля': 4, 'мая': 5, 'июня': 6, 'июля': 7, 'августа': 8,
-          'сентября': 9, 'октября': 10, 'ноября': 11, 'декабря': 12}
+PATH_TO_TASKS_FILE = 'tasks_file.json'
 
-
-def date_processing(date):
-    date_list = re.findall(
-        r'\d{2}.\d{2}.\d{4}|\d.\d{2}.\d{4}|\d{2} [яфмиасонд]\w+[яа] \d{4}|\d [яфмиасонд]\w+[яа] \d{4}', date)
-    date = date_list[0]
-    date = re.split(r'[:./\s]', date)
-    try:
-        date = str(datetime(int(date[2]), int(date[1]), int(date[0])).date())
-    except ValueError:
-        date = str(datetime(int(date[2]), months[date[1]], int(date[0])).date())
-    return date
-
-
-def add_todo(date, task):
-    if date in tasks:
-        tasks[date].append(task)
-    else:
-        tasks[date] = []
-        tasks[date].append(task)
+bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=["help"])
-def reference(message):
+def get_help(message):
+    """Отправляет справку в чат пользователю
+    :param message: сообщение пользователя"""
     bot.send_message(message.chat.id, HELP)
 
 
 @bot.message_handler(commands=['add'])
-def add(message):
+def add_task(message):
+    """
+    Отправляет сообщение пользователю о добавлении задачи
+    :param message: запрос пользователя
+    """
     command = message.text.split(' - ', maxsplit=2)
+
     try:
         date = command[1].lower()
+
         if date == 'сегодня':
             date = str(datetime.today().date())
         elif date == 'завтра':
             date = str(datetime.today().date() + timedelta(1))
         else:
-            date = date_processing(date)
+            date = utils.get_processed_date(date)
+
         task = command[2]
-        add_todo(date, task)
+        utils.add_todo(PATH_TO_TASKS_FILE, date, task)
         text = 'Задача ' + task + ' добавлена на дату ' + date
         bot.send_message(message.chat.id, text)
+
     except IndexError:
         bot.send_message(message.chat.id, 'Введите команду в верном формате "/add - дата - задача"')
 
 
 @bot.message_handler(commands=['random'])
-def random(message):
+def add_random_task(message):
+    """
+    Отправляет сообщение пользователю о добавлении рандомной задачи
+    :param message: запрос пользователя
+    """
     date = str(datetime.now().date())
     task = choice(RANDOM_TASKS)
-    add_todo(date, task)
+
+    utils.add_todo(PATH_TO_TASKS_FILE, date, task)
+
     text = 'Задача ' + task + ' добавлена на дату ' + date
+
     bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(commands=['show'])
-def show(message):
+def show_tasks(message):
+    """
+    Отправляет сообщение пользователю с задачами на выбранную дату
+    :param message: запрос пользователя
+    """
     command = message.text.split(' - ', maxsplit=1)
+
     try:
         date = command[1].lower()
+
         if date == 'сегодня':
             date = str(datetime.today().date())
         elif date == 'завтра':
             date = str(datetime.today().date() + timedelta(1))
         else:
-            date = date_processing(date)
+            date = utils.get_processed_date(date)
+
+        tasks = utils.open_file_with_tasks(PATH_TO_TASKS_FILE)
+
         if date in tasks:
             text = date.upper() + '\n'
+
             for task in tasks[date]:
                 text = text + '- ' + task + '\n'
         else:
             text = 'На данную дату нет доступных задач'
+
         bot.send_message(message.chat.id, text)
+
     except IndexError:
         bot.send_message(message.chat.id, 'Введите команду в верном формате "/show - дата"')
 
